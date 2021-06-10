df1eed4454fdd09a0ca7b765028b39ac39e660fb
Víctor Ferrando <victorferrandofoto@gmail.com>
2021-06-10 17:47:10 +0200

    One Plus 600 New changes

--- one/ilimitada-avanzada-300/js/main.js
+++ one/ilimitada-avanzada-300/js/main.js
@@ -1,5 +1,5 @@
 /**
- * Vodafone Reboot Framework: Version 2.15.0. Generation Date: 2021-06-10T14:59:53.837Z
+ * Vodafone Reboot Framework: Version 2.15.0. Generation Date: 2021-06-10T15:18:09.602Z
  */
 
 /******/ (function(modules) { // webpackBootstrap
