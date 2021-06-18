71eb22d45b3abda4e9f124882c9e745135df1cab
Víctor Ferrando <victorferrandofoto@gmail.com>
2021-06-18 10:52:51 +0200

    SummerDelight - Filters

--- summerdelight/js/main.js
+++ summerdelight/js/main.js
@@ -1,5 +1,5 @@
 /**
- * Vodafone Reboot Framework: Version 2.15.0. Generation Date: 2021-06-18T08:17:13.473Z
+ * Vodafone Reboot Framework: Version 2.15.0. Generation Date: 2021-06-18T08:47:52.080Z
  */
 
 /******/ (function(modules) { // webpackBootstrap
