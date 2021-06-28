aa7ee95e5397259033b8ba14efab5e87f1c5dd7a
Paloma Elena <palomaelena@MacBook-Pro-de-Paloma.local>
2021-06-28 21:12:13 +0200

    600plus norte

--- destiny4.0/Norte/ilimitada_avanzada_300/js/main.js
+++ destiny4.0/Norte/ilimitada_avanzada_300/js/main.js
@@ -1,5 +1,5 @@
 /**
- * Vodafone Reboot Framework: Version 2.17.0. Generation Date: 2021-06-28T15:48:44.851Z
+ * Vodafone Reboot Framework: Version 2.17.0. Generation Date: 2021-06-28T19:09:43.359Z
  */
 
 /******/ (function(modules) { // webpackBootstrap
--- destiny4.0/Norte/ilimitada_avanzada_600/js/main.js
+++ destiny4.0/Norte/ilimitada_avanzada_600/js/main.js
@@ -1,5 +1,5 @@
 /**
- * Vodafone Reboot Framework: Version 2.17.0. Generation Date: 2021-06-28T16:25:29.716Z
+ * Vodafone Reboot Framework: Version 2.17.0. Generation Date: 2021-06-28T19:06:24.227Z
  */
 
 /******/ (function(modules) { // webpackBootstrap
